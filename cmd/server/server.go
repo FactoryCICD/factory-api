4fd4acabc64b1870c3dd7a8ca9cd2bf61ff1c115
Josue Morales <molinajosue92@hotmail.com>
2024-03-06 00:06:23 -0500

    skeleton application with golang

---
+++ cmd/server/server.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"net/http"
+
+	dbcontext "github.com/FactoryCICD/factory-api/pkg/db"
+	"github.com/FactoryCICD/factory-api/pkg/log"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
+)
+
+// Build creates a new router and adds the routes to it
+func Build(logger log.Logger, db *dbcontext.DB) *chi.Mux {
+	r := chi.NewRouter()
+
+	// Middlewares
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	// Routes
+	r.Get("/", rootRoute)
+
+	// Handlers
+
+	// Print routes
+	printEstablishedRoutes(r, logger)
+	return r
+}
+
+func rootRoute(w http.ResponseWriter, r *http.Request) {
+	message := struct {
+		Message string `json:"message"`
+	}{Message: "Hello World"}
+	render.JSON(w, r, message)
+}
+
+func printEstablishedRoutes(r *chi.Mux, logger log.Logger) {
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		logger.Info("Route Established: ", method, " - ", route)
+		return nil
+	}
+	if err := chi.Walk(r, walkFunc); err != nil {
+		logger.Error("Error while printing routes: ", err.Error())
+	}
+}
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -8,13 +8,13 @@ if ! [ -x "$(command -v psql)" ]; then
     exit 1
 fi
 
-if ! [ -x "$(command -v sqlx)" ]; then
-    echo >&2 "Error sqlx is not installed."
-    echo >&2 "Use: "
-    echo >&2 "     cargo install sqlx-cli --no-default-features --features rustls,postgres"
-    echo >&2 "to install it."
-    exit 1
-fi
+# if ! [ -x "$(command -v sqlx)" ]; then
+#     echo >&2 "Error sqlx is not installed."
+#     echo >&2 "Use: "
+#     echo >&2 "     cargo install sqlx-cli --no-default-features --features rustls,postgres"
+#     echo >&2 "to install it."
+#     exit 1
+# fi
 
 DB_USER=postgres
 DB_PASSWORD=password
@@ -45,7 +45,7 @@ done
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 
-sqlx database create
-sqlx migrate run
+# sqlx database create
+# sqlx migrate run
 
->&2 echo "Postgres has been migrated, ready to go!"
+# >&2 echo "Postgres has been migrated, ready to go!"
